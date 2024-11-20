26ef91acd623e78a0c7f9fb35fe8fa988accbac0
HawoonShin <sinhaoon@gmail.com>
2024-11-20 16:35:16 +0900

    [Feat] 플레이어 이동
    플레이어가 바라보는 방향으로 이동합니다.

---
+++ Assets/Develop/SHW/Scripts/PlayerStatus.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour 
+{
+    //[SerializeField] public float speed { get; set; }    // ÇÃ·¹ÀÌ¾î ¼Óµµ
+    //[SerializeField] public float power { get; set; }     // ÆøÅº À§·Â
+    //[SerializeField] public int bombCount { get; set; }   // ÆøÅº¼ö
+}

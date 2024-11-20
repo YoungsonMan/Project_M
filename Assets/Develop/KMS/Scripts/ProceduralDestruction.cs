dbcb676d7e3430141437c3844158604b40521c88
Geniards <geniard93@gmail.com>
2024-11-20 16:56:31 +0900

    [Feat] 큐브 파괴시 원본삭제 및 파편 위치 변경.

--- Assets/Develop/KMS/Scripts/ProceduralDestruction.cs
+++ Assets/Develop/KMS/Scripts/ProceduralDestruction.cs
@@ -9,6 +9,7 @@ public class ProceduralDestruction : MonoBehaviour
     public float explosionForce = 20f;      // 폭발범위
     public float explosionRadius = 5f;      // 폭발반경
     public GameObject fragmentPrefab;       // 부서진 조각
+    public Transform parentContainer;       // 부서진 조각들을 담을 곳
 
     /// <summary>
     /// 원본 오브젝트가 사라지고 부서진 파편이 폭발로 날아갈 메서드.
@@ -27,6 +28,8 @@ public class ProceduralDestruction : MonoBehaviour
                 rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
             }
         }
+
+        Destroy(gameObject);
     }
 
     /// <summary>
@@ -42,7 +45,7 @@ public class ProceduralDestruction : MonoBehaviour
             //      Random.insideUnitSphere = 반경 1을 갖는 구 안의 임의의 지점을 반환합니다.
             Vector3 randomPos = transform.position + Random.insideUnitSphere * 0.5f;
             GameObject fragment = Instantiate(fragmentPrefab, randomPos, Random.rotation); //Rpc로 변경 예정.
-
+            
             // 조각의 크기를 무작위로 조정(다양성 부여)
             float randomScale = Random.Range(0.1f, 0.4f);
             fragment.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
@@ -50,6 +53,12 @@ public class ProceduralDestruction : MonoBehaviour
             // 조각에 물리적 속성 추가
             Rigidbody rb = fragment.AddComponent<Rigidbody>();
             rb.mass = 0.1f;
+
+            // 부모 오브젝트 설정
+            if (parentContainer)
+            {
+                fragment.transform.SetParent(parentContainer);
+            }
 
             // 생성된 조각을 리스트로 반환.
             fragments.Add(fragment);

e2f687dddc95b1b96bceeb4cea8dfaf27cb8bb16
KimSesame <kimbs082787@gmail.com>
2024-11-21 12:29:44 +0900

    [Feat]: 물풍선 중복 설치 불가

--- Assets/Develop/KHJ/Scripts/WaterBomb.cs
+++ Assets/Develop/KHJ/Scripts/WaterBomb.cs
@@ -6,6 +6,7 @@ using UnityEngine.Pool;
 public class WaterBomb : MonoBehaviour
 {
     [SerializeField] private float _lifeTime;
+    [SerializeField] private LayerMask _layerMask;
 
     private WaitForSeconds _delay;
     private IObjectPool<WaterBomb> _objectPool;
@@ -33,14 +34,25 @@ public class WaterBomb : MonoBehaviour
 
 
     /// <summary>
-    /// 물폭탄이 설치될 위치를 지정합니다.
+    /// 물풍선이 설치될 위치를 지정합니다.
     /// </summary>
-    /// <param name="placerPosition">폭탄을 설치하는 오브젝트의 position</param>
-    public void SetLocation(Vector3 placerPosition)
+    /// <param name="placerPosition">풍선을 설치하는 오브젝트의 position</param>
+    /// <returns>해당 위치에 물풍선 설치 가능 여부</returns>
+    public bool SetLocation(Vector3 placerPosition)
     {
         Vector3 offset = new(0, 0.5f, 0);
         Vector3 location = new Vector3(Mathf.RoundToInt(placerPosition.x), 0, Mathf.RoundToInt(placerPosition.z)) + offset;
 
+        // Inspect validation of location
+        Collider[] others = Physics.OverlapSphere(location, 0.3f, _layerMask);
+        if (others.Length > 0)
+        {
+            _objectPool.Release(this);
+            return false;
+        }
+
+        // Move to location
         transform.position = location;
+        return true;
     }
 }

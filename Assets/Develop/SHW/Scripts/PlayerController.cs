535020276ca29e58a8d14543b05012dc18e86170
HawoonShin <sinhaoon@gmail.com>
2024-11-21 10:42:18 +0900

    [Refator] 이동 코드 수정
    rigidbody를 추가하여 이전과 같은 기능을 하되 물리가 적용되는 이동으로 수정하였습니다.

--- Assets/Develop/SHW/Scripts/PlayerController.cs
+++ Assets/Develop/SHW/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float power;       // 폭탄파워
     [SerializeField] int bombCount;     // 폭탄수 
 
+    [SerializeField] Rigidbody rigid;
+
     [SerializeField] Animator animator;
 
     private void Start()
@@ -17,7 +19,11 @@ public class PlayerController : MonoBehaviour
     {
         // 플레이어 소유권자 일경우
         Move();
-        // TODO : 폭탄 설치
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SetBoom();
+        }
     }
 
     public void Move()
@@ -26,6 +32,7 @@ public class PlayerController : MonoBehaviour
         moveDir.x = Input.GetAxisRaw("Horizontal");
         moveDir.z = Input.GetAxisRaw("Vertical");
 
+        // 이동시 애니메이션 출력
         if (moveDir.x != 0 || moveDir.z != 0)
         {
             animator.SetBool("Move", true);
@@ -35,22 +42,29 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("Move", false);
         }
 
-        // 움직이지 않았을 시 
-        if (moveDir == Vector3.zero)
-            return;
-
+        // 계속 이동 방지
+        if (moveDir.magnitude < 0.1)
+        {
+            rigid.velocity = Vector3.zero;
+        }
 
         // 동시 입력 시 다른 방향 움직임 제한
-        if(moveDir.x !=0)
+        if (moveDir.x != 0)
         {
             moveDir.z = 0;
         }
-        else if(moveDir.z != 0) 
+        else if (moveDir.z != 0)
         {
             moveDir.x = 0;
         }
 
-        transform.Translate(moveDir.normalized * speed * Time.deltaTime, Space.World);
-        transform.forward = moveDir.normalized;
+        // 리지드 바디로 이동
+        rigid.velocity = moveDir.normalized * speed;
+        transform.forward = moveDir;
+    }
+
+    public void SetBoom()
+    {
+        // TODO : 폭탄 설치
     }
 }

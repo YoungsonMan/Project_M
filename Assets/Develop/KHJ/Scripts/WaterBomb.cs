8a27e7627e6f6122c5bf47649bca55203be4bb90
KimSesame <kimbs082787@gmail.com>
2024-11-21 11:41:50 +0900

    [Feat]: 물풍선 오브젝트 풀링 적용

--- Assets/Develop/KHJ/Scripts/WaterBomb.cs
+++ Assets/Develop/KHJ/Scripts/WaterBomb.cs
@@ -1,13 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class WaterBomb : MonoBehaviour
 {
     [SerializeField] private float _lifeTime;
 
+    private WaitForSeconds _delay;
+    private IObjectPool<WaterBomb> _objectPool;
+
+    public IObjectPool<WaterBomb> ObjectPool { set { _objectPool = value; } }
+
     private void OnEnable()
     {
-        Destroy(gameObject, _lifeTime);
+        Deactivate();
+    }
+
+    private void Start()
+    {
+        _delay = new WaitForSeconds(_lifeTime);
+    }
+
+    private void Deactivate() => StartCoroutine(DeactivateRoutine());
+
+    IEnumerator DeactivateRoutine()
+    {
+        yield return _delay;
+
+        _objectPool.Release(this);
+    }
+
+
+    /// <summary>
+    /// 물폭탄이 설치될 위치를 지정합니다.
+    /// </summary>
+    /// <param name="placerPosition">폭탄을 설치하는 오브젝트의 position</param>
+    public void SetLocation(Vector3 placerPosition)
+    {
+        transform.position = placerPosition;
     }
 }
---
+++ Assets/Develop/KHJ/Scripts/WaterBombPlacer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class WaterBombPlacer : MonoBehaviour
+{
+    [Header("WaterBomb Object Pool")]
+    [Tooltip("WaterBomb prefab to place")]
+    [SerializeField] private WaterBomb _waterBombPrefab;
+    [Tooltip("Throw an exception if we try to return an exisitng item, already in the pool")]
+    [SerializeField] private bool _collectionCheck = true;
+    [Tooltip("Initial pool size")]
+    [SerializeField] private int _defaultCapacity = 8;
+    [Tooltip("Maximum pool size")]
+    [SerializeField] private int _maxPoolSize = 8;
+    
+    private ObjectPool<WaterBomb> _waterBombPool;
+
+    private void Awake()
+    {
+        _waterBombPool = new ObjectPool<WaterBomb>(CreateWaterBomb,
+            OnGetFromPool, OnReleaseToPool, OnDestroyPooledObject,
+            _collectionCheck, _defaultCapacity, _maxPoolSize);
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < _maxPoolSize; i++)
+        {
+            CreateWaterBomb();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _waterBombPool != null && _waterBombPool.CountAll == 0)
+        {
+            // Get waterbomb from pool
+            WaterBomb waterBomb = _waterBombPool.Get();
+            if (waterBomb == null)
+                return;
+            
+            waterBomb.SetLocation(transform.position);
+        }
+    }
+
+    #region ObjectPool Callbacks
+    private WaterBomb CreateWaterBomb()
+    {
+        WaterBomb waterBomb = Instantiate(_waterBombPrefab);
+        waterBomb.ObjectPool = _waterBombPool;
+        return waterBomb;
+    }
+
+    private void OnGetFromPool(WaterBomb pooledObject) => pooledObject.gameObject.SetActive(true);
+    private void OnReleaseToPool(WaterBomb pooledObject) => pooledObject.gameObject.SetActive(false);
+    private void OnDestroyPooledObject(WaterBomb pooledObject) => Destroy(pooledObject.gameObject);
+    #endregion
+}

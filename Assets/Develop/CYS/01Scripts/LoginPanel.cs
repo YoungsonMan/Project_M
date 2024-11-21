8a43fa728ba8c8a12a7fa3d5419e25ee73137615
YoungsonMan <choi13570@naver.com>
2024-11-21 10:58:25 +0900

    [Chore] UI_Object들 인스펙터 조정
    * InputField들 ContentType
        - EmailAddress
        - Password
    로 각자 맞게끔 변경
    
    * UI_Object 불러올때 중복되는이름으로 안되는것들 다 중복 안되게 처리

--- Assets/Develop/CYS/01Scripts/LoginPanel.cs
+++ Assets/Develop/CYS/01Scripts/LoginPanel.cs
@@ -17,10 +17,16 @@ public class LoginPanel : BaseUI
     [SerializeField] Image _verificationPanel;
 
 
+    [SerializeField] GameObject _signUpPanel;
+
     private void OnEnable()
     {
         Init();
     }
+  //  private void Start()
+  //  {
+  //      Init();
+  //  }
     private void Init()
     {
         // TMP_Text
@@ -29,15 +35,17 @@ public class LoginPanel : BaseUI
         
         // TMP_InputField
         _emailInputField = GetUI<TMP_InputField>("IDInputField");
+        GetUI<TMP_Text>("IDInputPlaceholder").text = "example@gmail.com";
         _pwInputField = GetUI<TMP_InputField>("PWInputField");
-        
+        GetUI<TMP_Text>("PWInputPlaceholder").text = "Type your password in";
+
         // Button
-        GetUI<Button>("LoginButton");
-        GetUI<Button>("SignUpButton");
+        GetUI<Button>("LoginButton").onClick.AddListener(Login);
+        GetUI<Button>("SignUpButton").onClick.AddListener(GoToSignUp);
         GetUI<Button>("ResetPWButton");
 
         // VerificationPanel
-        _verificationPanel = GetUI<Image>("VerifcationPanel");
+        _verificationPanel = GetUI<Image>("VerificationPanel");
         GetUI<TMP_Text>("WaitingText");
         GetUI<Button>("VerificationCancelButton");
 
@@ -47,12 +55,32 @@ public class LoginPanel : BaseUI
         GetUI<TMP_InputField>("NicknameInputField");
         GetUI<Button>("ConfirmButton");
 
+        _signUpPanel = GetUI("SignUpPanel");
     }
 
+    public void SubscirbesEvents()
+    {
+       // GetUI<Button>("LoginButton").onClick.AddListener(Login);
+    }
+    public void GoToSignUp()
+    {
+        Debug.Log("회원가입 창 열기");
+        _signUpPanel.SetActive(true);
+    }
     public void Login()
     {
+        Debug.Log("LoginButton 테스트 로그");
+        
         string email = _emailInputField.text;
+        if (email == "")
+        {
+            Debug.Log("Email is empty, please put your eamil");
+        }
         string password = _pwInputField.text;
+        if (password == "")
+        {
+            Debug.Log("Password is empty, please put your eamil");
+        }
 
         
         BackendManager.Auth.SignInWithEmailAndPasswordAsync(email, password)
@@ -73,6 +101,7 @@ public class LoginPanel : BaseUI
                Debug.Log($"User signed in successfully: {result.User.DisplayName} ({result.User.UserId})");
                CheckUserInfo();
            });
+        
     }
     public void CheckUserInfo()
     {

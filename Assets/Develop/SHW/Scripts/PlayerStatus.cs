51a630c70ba6a6e03ba66b472575f737a3084800
HawoonShin <sinhaoon@gmail.com>
2024-11-22 11:29:03 +0900

    [Refactor] 플레이어 스테이터스 주석 수정
    플레이어 스테이터스 스크립트 사용하지 않는 부분들 수정

--- Assets/Develop/SHW/Scripts/PlayerStatus.cs
+++ Assets/Develop/SHW/Scripts/PlayerStatus.cs
@@ -7,29 +7,4 @@ public class PlayerStatus : MonoBehaviour
     [SerializeField] public int bombCount;     // ÆøÅº¼ö 
 
     public bool isBubble;
-
-    private float preSpeed;
-
-    private void Awake()
-    {
-       //preSpeed = speed;
-    }
-
-    private void Update()
-    {
-        Debug.Log($"{speed}");
-        //if (isBubble == false)
-        //{
-        //    speed = preSpeed;
-        //}
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        //if(collision.gameObject.name == "test")
-        //{
-        //    preSpeed = speed;
-        //    speed = 0.5f;
-        //}
-    }
 }

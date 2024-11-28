6a24eeaec8ad3e198ce81746c25ac030f0f31d2d
KimSesame <kimbs082787@gmail.com>
2024-11-28 13:17:25 +0900

    [Fix]: 출입가능 오브젝트 내부에서도 물줄기에 피격되도록 수정

--- Assets/Develop/KHJ/Scripts/EnterableObject.cs
+++ Assets/Develop/KHJ/Scripts/EnterableObject.cs
@@ -1,14 +1,24 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnterableObject : MonoBehaviourPun
 {
+    [SerializeField] List<GameObject> _innerObjs;
+
+    private void Awake()
+    {
+        _innerObjs = new List<GameObject>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Transform root = GetRoot(other.transform);
         List<Renderer> renderers = root.GetComponent<RendererCache>().Cache;
+
+        _innerObjs.Add(root.gameObject);
 
         if (renderers != null)
             Conceal(renderers);
@@ -19,8 +29,26 @@ public class EnterableObject : MonoBehaviourPun
         Transform root = GetRoot(other.transform);
         List<Renderer> renderers = root.GetComponent<RendererCache>().Cache;
 
+        _innerObjs.Remove(root.gameObject);
+
         if (renderers != null)
             Reveal(renderers);
+    }
+
+    private void OnDestroy()
+    {
+        if (_innerObjs.Count == 0)
+            return;
+
+        foreach (GameObject obj in _innerObjs)
+        {
+            List<Renderer> renderers = obj.GetComponent<RendererCache>().Cache;
+            if (renderers != null)
+                Reveal(renderers);
+
+            IExplosionInteractable interactable = obj.GetComponent<IExplosionInteractable>();
+            interactable?.Interact();
+        }
     }
 
     private Transform GetRoot(Transform target)
